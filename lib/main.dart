af4dbb17db6615eddee231a60fc7f38c52310a62
LÓPEZ H <mario.lopez22@itca.edu.sv>
2023-10-22 21:19:58 -0600

    refactorización de código y corrección de errores

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 
 class Todo {
   final String title;
@@ -10,15 +9,16 @@ class Todo {
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: 'Passing Data',
     home: TodosScreen(
-      todos: List.generate(
-        20,
-        (i) => Todo(
-              'Todo $i',
-              'A description of what needs to be done for Todo $i',
-            ),
-      ),
+      todos: [
+        Todo('Comprar leche', 'Comprar leche en la tienda'),
+        Todo('Llevar el coche al taller', 'Programar una revisión del coche'),
+        Todo('Estudiar para el examen', 'Preparar para el examen de matemáticas'),
+        Todo('Hacer ejercicio', 'Ir al gimnasio y hacer ejercicios'),
+        Todo('Cita con el médico', 'Consulta médica a las 3 PM'),
+      ],
     ),
   ));
 }
@@ -32,16 +32,13 @@ class TodosScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todos'),
-      ),
+        title: Text('Formas de pasar parametros de una pantalla a otra', style: TextStyle(fontSize: 16),
+      ),),
       body: ListView.builder(
         itemCount: todos.length,
         itemBuilder: (context, index) {
           return ListTile(
             title: Text(todos[index].title),
-            // Cuando un usuario pulsa en el ListTile, navega al DetailScreen.
-            // Tenga en cuenta que no solo estamos creando un DetailScreen,
-            // también le pasamos el objeto Todo actual!
             onTap: () {
               Navigator.push(
                 context,
@@ -58,15 +55,12 @@ class TodosScreen extends StatelessWidget {
 }
 
 class DetailScreen extends StatelessWidget {
-  // Declara un campo que contenga el objeto Todo
   final Todo todo;
 
-  // En el constructor, se requiere un objeto Todo
   DetailScreen({Key? key, required this.todo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Usa el objeto Todo para crear nuestra UI
     return Scaffold(
       appBar: AppBar(
         title: Text(todo.title),
@@ -75,105 +69,6 @@ class DetailScreen extends StatelessWidget {
         padding: EdgeInsets.all(16.0),
         child: Text(todo.description),
       ),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 137, 104, 237)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Formas de pasar parametros de una pantalla a otra'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
- 
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
