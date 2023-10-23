f5269c524e7a01c2de13b2ce53f1f5f5c44bd930
LÓPEZ H <mario.lopez22@itca.edu.sv>
2023-10-22 21:02:29 -0600

    Moestrar una Lista de objetos Todos usando una ListView

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,42 @@ void main() {
     ),
   ));
 }
+
+class TodosScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodosScreen({Key key, @required this.todos}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todos'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            // Cuando un usuario pulsa en el ListTile, navega al DetailScreen.
+            // Tenga en cuenta que no solo estamos creando un DetailScreen,
+            // también le pasamos el objeto Todo actual!
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
